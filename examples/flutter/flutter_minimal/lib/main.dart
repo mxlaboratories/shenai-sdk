e899585edf2ca95e1082de297588e92ba939af51
miloszmaki <miloszmaki@gmail.com>
2025-04-01 07:13:59 +0200

    upgrade Flutter example

--- examples/flutter/flutter_minimal/lib/main.dart
+++ examples/flutter/flutter_minimal/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shenai_sdk/pigeon.dart';
 import 'package:shenai_sdk/shenai_sdk.dart';
@@ -8,50 +9,33 @@ const String shenApiKey = "YOUR_API_KEY";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  
-  var res = await ShenaiSdk.initialize(shenApiKey, "");
+  await ShenaiSdk.initialize(shenApiKey, "");
+  await ShenaiSdk.setCustomMeasurementConfig(CustomMeasurementConfig(
+    instantMetrics: [Metric.heartRate,Metric.systolicBp,Metric.diastolicBp],
+    summaryMetrics: [Metric.age],
+  ));
   ShenaiSdk.setEventCallback((event) {
-    print("Shen.AI event: " + event.toString());
+    print("Shen.AI event: $event");
   });
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({super.key, required this.title});
   final String title;
 
   @override
@@ -59,8 +43,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   bool _isInitialized = true;
 
   String _title = "Shen.ai SDK";
@@ -70,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    timer = Timer.periodic(Duration(seconds: 1), (Timer t) async {
+    timer = Timer.periodic(const Duration(seconds: 1), (Timer t) async {
       var res = await ShenaiSdk.getMeasurementState();
       var results = await ShenaiSdk.getMeasurementResults();
       if (results != null && results.systolic_blood_pressure_mmhg != null) {
@@ -83,7 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
           _title = res.toString();
         });
       }
-
     });
   }
 
@@ -93,31 +74,11 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(_title),        
-        // We want to execute an action when title was clicked
         leading: GestureDetector(
           onTap: () async { 
             if (await ShenaiSdk.isInitialized()) {
@@ -155,16 +116,10 @@ class _MyHomePageState extends State<MyHomePage> {
               print(risks.scores.ageScore);
             }
           },
-          child: Icon(
-            Icons.menu,
-          ),
+          child: const Icon(Icons.menu),
         ),
       ),
-      body: Center(         
-        child: _isInitialized ? ShenaiView() : Text("Not initialized"),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-      ),
+      body: Center(child: _isInitialized ? ShenaiView() : const Text("Not initialized")),
     );
   }
 }
